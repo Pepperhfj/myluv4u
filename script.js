1009ee22c673857376d0ad48cc83e46fd1cb820c
CollectionofEG <148367269+Pepperhfj@users.noreply.github.com>
2025-06-17 17:36:22 +0700

    Update script.js

--- script.js
+++ script.js
@@ -17,7 +17,6 @@ const image1 = document.getElementById('image1');
 const image2 = document.getElementById('image2');
 
 increaser.onclick = heartNormal;
-
 decreaser.onclick = heartBreak;
 
 function heartNormal() {
